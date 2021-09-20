af5de95814544d3ca5c316c6957d7d3240165c5a
WhiteKr <hanjisu2215g@gmail.com>
2021-09-21 01:36:22 +0900

    Add 10989.kt | Add 10998.cpp | Add 11021.kt | Add 11022.kt

---
+++ src/10/11021.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val T: Int = readLine()!!.toInt()
+    for (i in 1..T) {
+        val (A: Int, B: Int) = readLine()!!.split(" ").map { it.toInt() }
+        println("Case #$i: ${A + B}")
+    }
+}
